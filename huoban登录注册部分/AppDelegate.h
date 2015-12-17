27ab24ace1feaefb5060c950bccc7f563341694e
NewImprove <1171377895@qq.com>
2015-12-17 10:38:41 +0800

    Initial Commit

---
+++ huoban登录注册部分/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  huoban登录注册部分
+//
+//  Created by 马锦航 on 15/12/17.
+//  Copyright © 2015年 马锦航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ huoban登录注册部分/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  huoban登录注册部分
+//
+//  Created by 马锦航 on 15/12/17.
+//  Copyright © 2015年 马锦航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
